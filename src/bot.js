c29380fd1304595e81da1572bb999085fe990d48
v1sher <aleksey.s.krotenko@gmail.com>
2024-08-13 07:56:48 +0500

    Удаление лишнего

--- src/bot.js
+++ src/bot.js
@@ -9,45 +9,38 @@ dotenv.config();
 const bot = new Telegraf(config.get('TOKEN_TG_BOT'));
 
 const INTERVAL = timeToMilliseconds({ minutes: 1 });
-console.log('INTERVAL', INTERVAL);
 const WORKING_HOURS_START = 7;
 const WORKING_HOURS_END = 22;
 
 let intervalId;
 
-// Проверка на рабочие часы
 function isWorkingTime() {
   const currentHour = new Date().getHours();
   return currentHour >= WORKING_HOURS_START && currentHour < WORKING_HOURS_END;
 }
 
-// Отправка случайного вопроса
 function sendRandomQuestion(ctx) {
-  const chatId = ctx.chat.id; // Извлекаем chatId из ctx
   const question = getRandomQuestion(questions);
   ctx.reply(question);
 
-  // Перезапуск интервала
   if (intervalId) {
     clearInterval(intervalId);
   }
 
   intervalId = setInterval(() => {
     if (isWorkingTime()) {
-      sendRandomQuestion(ctx); // Передаем ctx, чтобы использовать chatId
+      sendRandomQuestion(ctx);
     }
   }, INTERVAL);
 }
 
-// Команда /start
 bot.start((ctx) => {
   ctx.reply("Бот запущен!");
-  sendRandomQuestion(ctx); // Передаем ctx вместо chatId
+  sendRandomQuestion(ctx);
 });
 
-// Команда /next
 bot.command('next', (ctx) => {
-  sendRandomQuestion(ctx); // Передаем ctx вместо chatId
+  sendRandomQuestion(ctx);
 });
 
 bot.launch();
